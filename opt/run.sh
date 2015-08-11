3663b4377b4d077899ae163a3e55c7d067ba4e7e
Tim Dettrick <t.dettrick@uq.edu.au>
2015-08-11 14:25:18 +1000

    Updating with changes made to DIT4C gatehouse config

--- opt/run.sh
+++ opt/run.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
-echo "set \$portal \"$PORTAL_URL\";" > /etc/nginx/conf.d/portal_url.conf
+export DOCKER_ADDR=${DOCKER_ADDR:-172.17.42.1}
+export GATEHOUSE_URL=${GATEHOUSE_URL:-"http://gatehouse:8080"}
+export MACHINESHOP_URL=${MACHINESHOP_URL:-"http://machineshop:8080"}
+
+if [[ "$PORTAL_URL" == "" ]]; then
+  echo "PORTAL_URL must be defined" 1>&2
+  exit 1
+fi
+
+cat > /etc/nginx/conf.d/variables.conf <<VARIABLES
+set \$portal "$PORTAL_URL";
+set \$docker "$DOCKER_ADDR";
+set \$gatehouse "$GATEHOUSE_URL";
+set \$machineshop "$MACHINESHOP_URL";
+VARIABLES
 
 nginx -t
 
